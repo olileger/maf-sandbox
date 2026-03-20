1eaecab22cfaf7d3894754c464fd41e4e215eed1
Olivier <olileger@users.noreply.github.com>
2026-03-20 16:46:22 +0000

    Hello World using MAF and OpenAI API

--- 01-hello_world.py
+++ 01-hello_world.py
@@ -1,3 +1,17 @@
 from dotenv import load_dotenv
+import asyncio
+import os
+from agent_framework.openai import OpenAIResponsesClient
+
 load_dotenv()
 
+async def main():
+    agent = OpenAIResponsesClient(api_key=os.getenv("OAI_API_KEY"),
+                                  model_id=os.getenv("OAI_MODEL_ID")).as_agent(
+        name="Test",
+        instructions="Tu es un agent qui écrit un poème sur un thème donné.",
+    )
+    result = await agent.run("Écris un poème sur le thème de l'amitié.")
+    print(result)
+
+asyncio.run(main())
